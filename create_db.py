d144ee7d4b9ab3fc9db7a5f15ced48cb66ce9dc2
mike-vestergaard <70510215+mike-vestergaard@users.noreply.github.com>
2021-09-09 23:55:56 +0200

    Preparation to establish db in MySQL but code not yet run
    Preparation to establish db in MySQL but code not yet run

--- create_db.py
+++ create_db.py
@@ -1,2 +1,20 @@
+import mysql.connector
+
 # This file will be deleted after run first time
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='password',
+)
+
+my_cursor = mydb.cursor()
+
+my_cursor.execute("CREATE DATABASE our_users")
+
+# To Inform if Database is actually created!
+my_cursor.execute("SHOW DATABASES")
+for db in my_cursor:
+    print(db)
+
 
