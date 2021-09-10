dcf0e97b58050a127773647a60c3d60cb72c957f
mike-vestergaard <70510215+mike-vestergaard@users.noreply.github.com>
2021-09-10 22:57:02 +0200

    hashing password but not yet linked to anything
    Hashing password Authentication to keep track of who does what however not yet linked to anything

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired, Email
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from werkzeug.security import  generate_password_hash, check_password_hash
 
 
 # Create a Flask Instance
@@ -33,6 +33,19 @@ class Users(db.Model):
     email = db.Column(db.String(120), nullable=False, unique=True)
     favorite_color = db.Column(db.String(120))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    #Hashing Password Authentication
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute!')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     # Create A String
     def __repr__(self):
