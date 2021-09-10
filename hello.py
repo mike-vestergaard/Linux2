cfbbd03c1c5a5d68f6d0ffeab65e25c891a14262
mike-vestergaard <70510215+mike-vestergaard@users.noreply.github.com>
2021-09-10 23:22:45 +0200

    hashed password functions for registration
    hashed passwords for registration and making migration so that password is in db

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, flash, request
 from flask.helpers import safe_join
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.fields.simple import PasswordField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -83,6 +84,8 @@ class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
     favorite_color = StringField("Favorite Color")
+    password_hash = PasswordField('Password', validators=[DataRequired(), EqualTo('password_hash2', message='Passwords Must Match!')])
+    password_hash2 = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 # New page to Updatabase Records
@@ -139,13 +142,19 @@ def add_user():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = Users(name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data)
+            # Hash Password
+            hashed_pw = generate_password_hash(form.password_hash.data, "sha256")
+            user = Users(name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data, 
+            password_hash=hashed_pw)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
         form.name.data = ''
         form.email.data = ''
         form.favorite_color.data = ''
+        form.password_hash = ''
+
+
         flash("User Added Successfully!")
     # This will return everything in the DB
     our_users = Users.query.order_by(Users.date_added)
