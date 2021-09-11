1d9e188c942a60115b2983a535d0be2ff3b0df56
mike-vestergaard <70510215+mike-vestergaard@users.noreply.github.com>
2021-09-11 09:17:01 +0200

    created blog posts page
    Display blog posts page

--- hello.py
+++ hello.py
@@ -46,6 +46,13 @@ class PostForm(FlaskForm):
     slug = StringField("Slugfield", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
+@app.route('/posts')
+def posts():
+    # Grab all the posts from the database
+    posts = Posts.query.order_by(Posts.date_posted)
+    return render_template("posts.html", posts=posts)
+
+
 # Add Post Page
 @app.route('/add-post', methods=['GET', 'POST'])
 def add_post():
